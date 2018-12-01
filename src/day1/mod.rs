72dc1f820b9eb3354a4f6470112f85cabe804b5d
Daniel Boyle <boylede@gmail.com>
2018-12-01 14:50:26 -0800

    download input if not cached

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,11 @@
+use std::fs::File;
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+
 use aoc2018::Day;
+
+use aoc2018::get_input_for_day;
 
 const DAY: i32 = 1;
 
@@ -8,5 +15,22 @@ pub fn load(days_array: &mut Vec<Day>) {
 
 pub fn run(optional_input:Vec<String>) {
 	// Day 1 code.
+let mut input_filename = match optional_input.len() {
+            0 => {
+            	println!("using default");
+            	get_input_for_day(DAY)
+            },
+            _ => {
+            	println!("using provided input, {}", optional_input);
+            	optional_input
+            },
+        };
+    let input_file = fs::OpenOptions::new().read(true).write(false).create(false).open(input_filename);
+
+	// if optional_input.len() > 0 {
+	// 	// todo: implement parsing optional input
+	// } else {
+	// 	// load the default input file
+	// }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+extern crate reqwest;
+
+use std::io::BufRead;
+use std::io::Write;
 use std::fmt;
+use std::fs;
+use std::io::BufReader;
+
+use reqwest::header::{HeaderMap, HeaderValue, HeaderName};
 
 #[derive(Debug)]
 pub struct Config {
@@ -40,4 +48,65 @@ impl fmt::Display for Day {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "Day #{}", self.index)
 	}
+}
+
+
+pub fn get_input_for_day(day: i32) -> String {
+	let file_path = format!("input/day{}.input", day);
+	let file = fs::OpenOptions::new().read(true).write(false).create(false).open(&file_path);
+
+	if let Err(_e) = file {
+		println!("Downloading inputs for this day.");
+		get_url_into_file("https://adventofcode.com/2018/day/1/input".to_string(), file_path);
+	} else {
+		println!("found input file");
+	}
+	// Err(NotFound) => println!("couldnt find file"),
+ //    Err(e) => panic!("Error while opening file:{}", e)
+
+    println!("loaded default file");
+	unimplemented!()
+}
+
+fn make_session_header() -> HeaderMap {
+	let session_file = fs::OpenOptions::new().read(true).write(false).create(false).open("session.txt").unwrap();
+	let mut session_reader = BufReader::new(session_file);
+	let mut session_raw = String::new();
+	let len = session_reader.read_line(&mut session_raw).unwrap();
+	session_raw = session_raw.trim_end().to_string();
+	let mut headers = HeaderMap::new();
+	if len > 0 {
+		println!("using cookie: \n{}", &session_raw);
+		let name = HeaderName::from_lowercase(b"cookie").unwrap();
+		let value = match HeaderValue::from_str(session_raw.trim_end()) {
+			Ok(c) => c,
+			Err(e) => panic!("Error with your session.txt file, {}", e),
+		};
+		headers.insert(
+			name,
+			value
+			);
+	}
+	headers
+}
+
+fn get_url(url:String) -> reqwest::Response {
+	let headers = make_session_header();
+	let request = reqwest::Client::new().get(&url).headers(headers).send();
+
+    let res = match request{
+            Ok(c) => c,
+            Err(e) => panic!("err:{}", e)
+        };
+    res
+}
+
+fn get_url_into_file(url: String,  filename: String) {
+    let mut request = get_url(url);
+    let mut file = match fs::OpenOptions::new().read(true).write(true).create(true).open(filename) {
+    	Ok(c) => c,
+        Err(e) => panic!("Error while opening file:{}", e)
+    };
+    request.copy_to(&mut file).unwrap();
+    file.flush().unwrap();
 }
