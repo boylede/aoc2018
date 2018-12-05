dd204d67f2a93e3ac2d429f7fa4ef5c3c714b796
Daniel Boyle <boylede@gmail.com>
2018-12-04 18:05:36 -0800

    stub day 3,4, fix warnings throughout

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,7 +1,6 @@
 extern crate time;
 
 use std::fs::File;
-use std::fs;
 use std::io::prelude::*;
 use std::io::{SeekFrom, Seek};
 use std::io::BufReader;
@@ -18,7 +17,7 @@ pub fn run(mut input: File) {
     let a_time = time::precise_time_ns();
     part1(&input);
     let b_time = time::precise_time_ns();
-    input.seek(SeekFrom::Start(0 as u64));
+    input.seek(SeekFrom::Start(0 as u64)).unwrap();
     part2(&input);
     let c_time = time::precise_time_ns();
     println!("Day 1 Part 1 took: {}ns", b_time - a_time);
@@ -27,11 +26,11 @@ pub fn run(mut input: File) {
 
 fn part1(input: &File) {
 	println!("Running Part 1");
-    let mut reader = BufReader::new(input);
+    let reader = BufReader::new(input);
     let mut accumulator = 0;
-    let mut operationCounter = 0;
-    for lineResult in reader.lines() {
-        if let Ok(mut line) = lineResult {
+    let mut operation_counter = 0;
+    for line_result in reader.lines() {
+        if let Ok(mut line) = line_result {
             let operation = line.get(0..1).unwrap();
             let value_string = line.get(1..).unwrap();
             let value = value_string.parse::<i32>().unwrap();
@@ -43,15 +42,15 @@ fn part1(input: &File) {
                     return;
                 },
             }
-            operationCounter += 1;
+            operation_counter += 1;
         }
     }
-    println!("Result: {}, after {} operations", accumulator, operationCounter);
+    println!("Result: {}, after {} operations", accumulator, operation_counter);
 }
 
 fn part2(input: &File) {
     println!("Running Part 2");
-    let mut reader = BufReader::new(input);
+    let reader = BufReader::new(input);
     let mut accumulator = 0;
     let mut accumulated: Vec<i32> = vec!();
 
@@ -62,12 +61,12 @@ fn part2(input: &File) {
         lines_vec.push(line);
     }
 
-    let mut lines_values: Vec<i32> = lines_vec.iter().map(|line| {
+    let lines_values: Vec<i32> = lines_vec.iter().map(|line| {
         let value = line.parse::<i32>().unwrap();
         value
     }).collect();
 
-    let values_iterator = Infinite_iter::from_vec(&lines_values);
+    let values_iterator = InfiniteIter::from_vec(&lines_values);
     for value in values_iterator {
         accumulator += value;
         if accumulated.contains(&accumulator) {
@@ -80,13 +79,13 @@ fn part2(input: &File) {
 
 }
 
-struct Infinite_iter<'a, T: 'a> {
+struct InfiniteIter<'a, T: 'a> {
     inner: &'a Vec<T>,
     pos: usize,
     loops: usize,
 }
 
-impl <'a, T> Infinite_iter<'a, T> {
+impl <'a, T> InfiniteIter<'a, T> {
     fn from_vec(vector: &'a Vec<T>) -> Self {
         Self {
             inner: vector,
@@ -96,7 +95,7 @@ impl <'a, T> Infinite_iter<'a, T> {
     }
 }
 
-impl<'a, T> Iterator for Infinite_iter<'a, T> {
+impl<'a, T> Iterator for InfiniteIter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         if self.pos >= self.inner.len() {
--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,4 +1,8 @@
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use std::collections::HashMap;
 
 use aoc2018::Day;
 
@@ -9,6 +13,35 @@ pub fn load(days_array: &mut Vec<Day>) {
 }
 
 pub fn run(input: File) {
-	// Day 3 code.
+	let a_time = time::precise_time_ns();
+	println!("loading day {} input.", DAY);
+	
+	let mut lines = vec!();
+	{
+		let mut lines_iterator = BufReader::new(&input).lines();
+	    while let Some(Ok(line)) = lines_iterator.next() {
+	        lines.push(line);
+	    }
+	}
+    let b_time = time::precise_time_ns();
+    println!("Loading took: {}ns", b_time - a_time);
+    post_load(lines);
+
 }
 
+fn post_load(lines: Vec<String>) {
+    let a_time = time::precise_time_ns();
+    part1(&lines);
+    let b_time = time::precise_time_ns();
+    part2(&lines);
+    let c_time = time::precise_time_ns();
+    println!("Day {} Part 1 took: {}ns", DAY, b_time - a_time);
+    println!("Day {} Part 2 took: {}ns", DAY, c_time - b_time);
+
+}
+fn part1(lines: &Vec<String>) {
+
+}
+fn part2(lines: &Vec<String>) {
+
+}
