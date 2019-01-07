040a618899440899055c4009090729a7b4f1bdd4
Daniel Boyle <boylede@gmail.com>
2019-01-06 16:31:16 -0800

    day 8 part 1

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -1,10 +1,11 @@
+use std::fmt::Display;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::num::ParseIntError;
 use std::str::FromStr;
 use std::collections::HashMap;
-
+use std::fmt::Formatter;
 
 use aoc2018::Day;
 
@@ -42,12 +43,134 @@ fn post_load(lines: Vec<String>) {
 
 }
 fn part1(lines: &Vec<String>) {
+	let license = lines.iter().next().unwrap().parse::<LicenseNode>();
+	if let Ok(license) = license {
+		// println!("got License: {:?}", license);
+		let mut total = 0;
+		license.visit_all(&mut |node: &LicenseNode| {
+			for num in &node.metadata {
+				total = total + num;
+			}
+		});
+		println!("total of all metadata: {}", total);
 
+	}
 }
-
 
 fn part2(lines: &Vec<String>) {
 
 }
 
+#[derive(Debug)]
+struct LicenseNode {
+	children: Vec<LicenseNode>,
+	metadata: Vec<u32>,
+}
+
+impl LicenseNode {
+	fn visit_all(&self, visitor: &mut FnMut(&LicenseNode) ) {
+		// LicenseDepthIter {
+		// 	l: self,
+		// 	index: 0,
+		// }
+		for child in &self.children {
+			child.visit_all(visitor);
+		}
+		visitor(&self);
+	}
+}
+
+impl FromStr for LicenseNode {
+	type Err = LicenseErr;
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let numbers = 
+			s
+			.split_whitespace()
+			.map(
+				|s:&str| {
+					s.parse::<u32>().unwrap()
+				})
+			.collect::<Vec<u32>>();
+		Ok(parse_node(&mut numbers.iter()))
+	}
+}
+
+fn parse_node(mut numbers: &mut std::slice::Iter<u32>) -> LicenseNode {
+	let node_count = numbers.next().unwrap();
+	let meta_count = numbers.next().unwrap();
+	let mut nodes = Vec::new();
+	for node_index in 0..*node_count {
+		nodes.push(parse_node(&mut numbers));
+	}
+	let mut metas : Vec<u32> = Vec::new();
+	for meta_index in 0..*meta_count {
+		metas.push(*numbers.next().unwrap());
+	}
+	LicenseNode{
+		children: nodes,
+		metadata: metas,
+	}
+}
+
+struct LicenseDepthIter<'a> {
+	l: &'a LicenseNode,
+	index: usize,
+}
+
+impl<'a> Iterator for LicenseDepthIter<'a> {
+	type Item = LicenseNode;
+	fn next(&mut self) -> Option<Self::Item> {
+		unimplemented!()
+	}
+}
+
+// struct Metadata {
+// 	value: i32,
+// }
+
+
+// struct License {
+// 	vec: Vec<u32>,
+// }
+
+enum LicenseErr {
+	Error,
+}
+// impl FromStr for License {
+// 	type Err = LicenseErr;
+// 	fn from_str(s: &str) -> Result<Self, Self::Err> {
+// 		let vec : Vec<u32> = 
+// 			s
+// 			.split_whitespace()
+// 			.map(
+// 				|s:&str| {
+// 					s.parse::<u32>().unwrap()
+// 				})
+// 			.collect::<Vec<u32>>();
+// 		Ok(License {
+// 			vec,
+// 		})
+// 	}
+// }
+
+// impl Display for License {
+// 	fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> { 
+// 		for number in &self.vec {
+// 			write!(f, "{} ", number);
+// 		}
+// 		Ok(())
+// 	}
+// }
+
+// impl<'a> License {
+// 	fn depth_iter(&'a self) -> LicenseDepthIter {
+// 		LicenseDepthIter {
+// 			l: self,
+// 			index: 0,
+// 		}
+// 	}
+// 	fn example(&self) {
+// 		println!("what?");
+// 	}
+// }
 
